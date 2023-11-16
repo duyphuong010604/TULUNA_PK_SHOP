7fe5fc39d50f8bfc39e1f06be0db5aa711456202
duyphuong010604 <voduyphuong13@gmail.com>
2023-11-16 08:43:00 +0700

    N1 - XX : CHINH SUA GIAO DIEN

---
+++ view/admin/login/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="../../../contents/admin/assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>ADMIN</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../../../uploads/images-logo/ADMIN.png" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../../../contents/admin/assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../../../contents/admin/assets/vendor/css/core.css"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../../../contents/admin/assets/vendor/css/theme-default.css"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../../../contents/admin/assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../../../contents/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <link rel="stylesheet" href="../../../contents/admin/assets/vendor/libs/apex-charts/apex-charts.css" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="../../../contents/admin/assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../../../contents/admin/assets/js/config.js"></script>
+</head>
+
+<body>
+   
+    <main class="mt-5 mb-5 ">
+        <div class="row mx-auto ">
+            <div class="col-md-4 col-sm-8 mx-auto p-5 backgound-login border border-info rounded ">
+                <div class="row mb-3">
+                    <div class="top-image col-12 d-flex justify-content-center align-items-center">      
+                        <img src="../../../uploads/images-logo/ADMIN.png" alt="">
+                        <p class='ms-2 text-login-admin'>
+                            Đăng nhập 
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <form>
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Tên đăng nhập</label>
+                                <input type="text" class="form-control" id="username"
+                                    >
+                                
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Đồng ý với điều khoản</label>
+                            </div>
+                            <div class="button-login">
+                                <a href="#"><button type="submit" class="btn btn-primary">Quên mật khẩu</button></a>
+                                <button type="submit" class="btn btn-success">Đăng nhặp</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+    
+</body>
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+<script src="../../../contents/admin/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="../../../contents/admin/assets/vendor/libs/popper/popper.js"></script>
+<script src="../../../contents/admin/assets/vendor/js/bootstrap.js"></script>
+<script src="../../../contents/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+<script src="../../../contents/admin/assets/vendor/js/menu.js"></script>
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="../../../contents/admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+<!-- Main JS -->
+<script src="../../../contents/admin/assets/js/main.js"></script>
+
+<!-- Page JS -->
+<script src="../../../contents/admin/assets/js/dashboards-analytics.js"></script>
+
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
---
+++ view/client/login/index.php
@@ -0,0 +1,64 @@
+
+
+    <!-- Start Banner Area -->
+    <section class="banner-area organic-breadcrumb">
+        <div class="container">
+            <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+                <div class="col-first">
+                    <h1>Đăng nhập/Đăng ký</h1>
+                    <nav class="d-flex align-items-center">
+                        <a href="index.html">Trang chủ<span class="lnr lnr-arrow-right"></span></a>
+                        <a href="category.html">Đăng nhập</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Banner Area -->
+
+    <!--================Login Box Area =================-->
+    <section class="login_box_area section_gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="login_box_img">
+                        <img class="img-fluid" src="../../contents/client/img/login.jpg" alt="">
+                        <div class="hover">
+                            <p>Đăng ký tài khoảng để có thể mua hàng một cách nhanh chóng!</p>
+                            <a class="primary-btn" href="../../view/client/index.php?atc=register">Tạo tài khoản</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="login_form_inner">
+                        <h3>Đăng Nhập</h3>
+                        <form class="row login_form" action="contact_process.php" method="post" id="contactForm"
+                            novalidate="novalidate">
+                            <div class="col-md-12 form-group">
+                                <input type="text" class="form-control" id="name" name="name"
+                                    placeholder="Tên tài khoản" onfocus="this.placeholder = ''"
+                                    onblur="this.placeholder = 'Username'">
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Mật khẩu"
+                                    onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <div class="creat_account">
+                                    <input type="checkbox" id="f-option2" name="selector">
+                                    <label for="f-option2">Ghi nhớ đăng nhập</label>
+                                </div>
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <button type="submit" value="submit" class="primary-btn">Đăng Nhập</button>
+                                <a href="quenmatkhau.html">Quên mật khẩu?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================End Login Box Area =================-->
+
+    
